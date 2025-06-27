db76aa03318e7479831a3c1e1a5e5510d877074f
JustinAlexander <c14220144@john.petra.ac.id>
2025-06-27 13:12:16 +0700

    Commit

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'screens/splash_screen.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Supabase.initialize(
-    url: 'https://jbrdwjgkcfelmaqrygyt.supabase.co',
+    url: 'https://jbrdwjgkcfelmaqrygyt.supabase.co', //Link Supabase
     anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpicmR3amdrY2ZlbG1hcXJ5Z3l0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA4NTgwOTIsImV4cCI6MjA2NjQzNDA5Mn0.6Bf9u4l7b6lVDWZrQKkpeRcsqPliOhhDVkB17d_fk80',
   );
   runApp(const MyApp());
